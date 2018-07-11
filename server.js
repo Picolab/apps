b1ecb4bfd56a5b6ca59f9ab97f23f72d05c91d49
cgrimm013 <cagrimm13@gmail.com>
2018-07-11 16:10:40 -0600

    Posting to /safeandmine/api/tags successfully creates table entry
    Co-Authored-By: kandarej <kandarej@users.noreply.github.com>

--- server.js
+++ server.js
@@ -28,9 +28,10 @@ let config = require('./knexfile')[env];
 prompt.get(schema, function (err, result) {
 
   config.connection.password = result.databasePassword;
-  
+
   const mysql = knex(config);
 
+  //This query is only used for the purpose of verifying the connection to the database. If the query fails, the application exits.
   mysql.raw('select 1+1 as result').then(() => {
     // there is a valid connection in the pool
     console.log("DB connection successful!");
@@ -42,7 +43,26 @@ prompt.get(schema, function (err, result) {
 
   //set up server
   app.post('/safeandmine/api/tags', (req, res) => {
-
+    const { tagID, DID } = req.body;
+    if(!tagID && !DID){
+      return res.status(400).send("Missing tagID or DID");
+    }
+    mysql('SafeAndMine').where('tagID', tagID).then((rows) => {
+      if(rows.length > 0) {
+        console.error("tagID is already registered.", tagID);
+        return res.status(400).send();//We don't want to provide information because we don't want attackers to know if they found a legitimate tag already registered.
+      }else {
+        mysql('SafeAndMine').insert({ tagID, DID }).then((result) => {
+          console.log("result", result);
+          res.status(200).json({
+            tagID,
+            DID
+          })
+        }).catch((e) => {
+          console.error(e);
+        });
+      }
+    });
   });
 
   app.get('/safeandmine/:tagID', (req, res) => {
